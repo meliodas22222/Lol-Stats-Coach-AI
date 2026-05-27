1b383fdf2fe2fbdbc179ff32c4613ae15ba4bbff
meliodas22222 <mattiamannu08@gmail.com>
2026-05-27 20:20:06 +0200

    Update summoner.js

--- api/summoner.js
+++ api/summoner.js
@@ -3,20 +3,23 @@ export default async function handler(req, res) {
   const apiKey = process.env.VITE_RIOT_API_KEY;
 
   try {
-    // 1. Otteniamo l'account (puuid)
     const accRes = await fetch(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(name)}/${encodeURIComponent(tag)}`, {
       headers: { "X-Riot-Token": apiKey }
     });
     const account = await accRes.json();
 
-    // 2. Otteniamo il summoner (per avere il livello e l'icona su LoL)
     const sumRes = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${account.puuid}`, {
       headers: { "X-Riot-Token": apiKey }
     });
     const summoner = await sumRes.json();
 
-    return res.status(200).json({ ...account, ...summoner });
+    const rankRes = await fetch(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.id}`, {
+      headers: { "X-Riot-Token": apiKey }
+    });
+    const rank = await rankRes.json();
+
+    return res.status(200).json({ ...account, ...summoner, rankData: rank });
   } catch (error) {
-    return res.status(500).json({ error: "Errore nel recupero dati" });
+    return res.status(500).json({ error: "Errore" });
   }
 }
