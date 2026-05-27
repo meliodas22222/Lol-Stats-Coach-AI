126c957f63f9dc8f97f0b23b997b32936e595499
meliodas22222 <mattiamannu08@gmail.com>
2026-05-27 19:28:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_RIOT_API_KEY': JSON.stringify('RGAPI-74c81461-9529-4e37-bd43-1090864f80d1')
+    'process.env.VITE_RIOT_API_KEY': JSON.stringify('process.env.VITE_RIOT_API_KEY')
   }
 })
