58bddc4b8ccccceaed5da3202c234940b4dff23b
meliodas22222 <mattiamannu08@gmail.com>
2026-05-27 20:25:59 +0200

    Update summoner.js

--- api/summoner.js
+++ api/summoner.js
@@ -6,13 +6,11 @@ export default async function handler(req, res) {
     const accRes = await fetch(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(name)}/${encodeURIComponent(tag)}`, {
       headers: { "X-Riot-Token": apiKey }
     });
-    if (!accRes.ok) return res.status(200).json({ error: "Account non trovato (" + accRes.status + ")" });
     const account = await accRes.json();
 
     const sumRes = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${account.puuid}`, {
       headers: { "X-Riot-Token": apiKey }
     });
-    if (!sumRes.ok) return res.status(200).json({ error: "Summoner non trovato" });
     const summoner = await sumRes.json();
 
     const rankRes = await fetch(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.id}`, {
@@ -20,8 +18,8 @@ export default async function handler(req, res) {
     });
     const rank = await rankRes.json();
 
-    return res.status(200).json({ ...account, ...summoner, rankData: rank });
+    return res.status(200).json({ account, summoner, rank });
   } catch (error) {
-    return res.status(200).json({ error: "Errore interno server" });
+    return res.status(500).json({ error: "Errore server" });
   }
 }
