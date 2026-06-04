be8bb880d4274a21b045ee701b5589b765b5d705
meliodas22222 <mattiamannu08@gmail.com>
2026-06-04 14:38:03 +0200

    Update summoner.js

--- api/summoner.js
+++ api/summoner.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const { name, tag } = req.query;
-  const RIOT_API_KEY = "RGAPI-f7e8628a-9a64-4c87-8e8c-24c8cba88ae8";
+  const RIOT_API_KEY = "RGAPI-aa2300c6-b765-4346-a8fd-fc33bc816efe";
 
   try {
     const acc = await (await fetch(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(name)}/${encodeURIComponent(tag)}?api_key=${RIOT_API_KEY}`)).json();
